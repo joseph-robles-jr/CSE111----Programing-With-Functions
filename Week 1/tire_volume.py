28e02bf7e46f65fac8c971900900397cda916d6e
joseph-robles-jr <rjosepha@byui.edu>
2024-02-29 16:54:59 -0700

    Added writing to files. Fixed math syntax.

--- Week 1/tire_volume.py
+++ Week 1/tire_volume.py
@@ -1,4 +1,6 @@
 import math
+from datetime import datetime
+
 # v = (math.pi() * w**2 *(w * a + 2540 * d)) / 10000000000
 # "v" = Volume in Liters
 # "math.pi" = 3.1415.....
@@ -6,9 +8,10 @@ import math
 # "a" = aspect ratio of the tire
 # "d" = is the diameter of the wheel 
 def calculate_voume_of_space(w,a,d,):
-    v = (math.pi * (w**2) *(w * a + 2540 * d)) / 10000000000
+    v = (math.pi * (w**2) * a * (w * a + 2540 * d)) / 10000000000
     return v
 
+time = datetime.now(tz=None)
 
 
 width_in_milimeters = int(input('What is the width in milimeters?: '))
@@ -20,4 +23,9 @@ diameter_of_wheel = int(input('What is the diameter of the wheel in inches?: '))
 
 volume_in_liters = calculate_voume_of_space(width_in_milimeters,aspect_ratio, diameter_of_wheel)
 
-print(volume_in_liters)
+print(f'The aproximate volume is {volume_in_liters:.2f} liters')
+
+#opens and writes variables to volumes.txt
+with open("volumes.txt", "at") as volumes_file:
+
+    print(f'{time:%y-%m-%d}, {width_in_milimeters}, {aspect_ratio}, {diameter_of_wheel}, {volume_in_liters:.2f}', file=volumes_file)
